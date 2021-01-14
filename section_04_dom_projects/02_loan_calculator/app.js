134689e1d84ea146773432101350bd9c29dfa1f7
Derek Hingeveld <derekhingeveld@dereks-mbp.lan>
2021-01-14 16:57:14 -0700

    started on the loan calculator

---
+++ section_04_dom_projects/02_loan_calculator/app.js
@@ -0,0 +1,9 @@
+// List for submit
+document.getElementsById('load-form').addEventListener('submit', calculateResults);
+
+// Calculate Results
+function calculateResults(e){
+  console.log('Calculating...')
+
+  e.preventDefault();
+}
